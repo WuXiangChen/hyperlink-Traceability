c778068f0be3c801716d73fd83af6cf2eb60b677
WuXiangChen <1527723662@qq.com>
2024-10-30 13:28:08 +0000

    squeeze the code file and make it focus on the comparative goals

--- kill_all_running_process.sh
+++ kill_all_running_process.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-processes=$(ps aux | grep main | grep -v grep | awk '{print $2}')
+processes=$(ps aux | grep "python main.py" | grep -v grep | awk '{print $2}')
 
 if [ -z "$processes" ]; then
     echo "没有找到包含'mian'的进程。"
 else
-    echo "找到以下进程包含'mian'，正在杀死它们："
+    echo "找到以下进程包含'python mian'，正在杀死它们："
     echo "$processes"
     for pid in $processes; do
         kill -9 $pid
